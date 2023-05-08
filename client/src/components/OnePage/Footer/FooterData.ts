f35a149c8b01a6eec4356f39eae31bb8681feb1c
kinga-kus <103138106+kinga-kus@users.noreply.github.com>
2023-05-08 20:27:10 +0200

    [AI-0011]: feat: changed Footer component (#10)
    * feat: changed Footer component
    
    * build: changed code according to the suggestions
    
    ---------
    
    Co-authored-by: Kinga Kuś <kingakus@jabuszko-kk.home>

---
+++ client/src/components/OnePage/Footer/FooterData.ts
@@ -0,0 +1,19 @@
+const FooterData = [
+  {
+    className: 'group-1',
+    name: 'CHATTIE',
+    subnames: ['Community', 'Contact'],
+  },
+  {
+    className: 'group-2',
+    name: 'Follow us',
+    subnames: ['Twitter', 'Instagram'],
+  },
+  {
+    className: 'group-3',
+    name: 'Content',
+    subnames: ['YouTube'],
+  },
+];
+
+export default FooterData;
